0445065b2519bbc81c9a7670037dd26ce059c471
abdullahqb57 <abdullahqb57@gmail.com>
2019-01-23 17:00:30 +0530

    my robo react proj

---
+++ README.md
@@ -0,0 +1,6 @@
+# robofriends
+To run my project:
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+// export const robots = [
+//   {
+//     id: 1,
+//     name: 'Mahesh',
+//     username: 'Nani',
+//     email: 'mahesh.dasari333@gmail.com'
+//   },
+//   {
+//     id: 2,
+//     name: 'Abdullah',
+//     username: 'Abu',
+//     email: 'abdullahqb57@gmail.com'
+//   },
+//   {
+//     id: 3,
+//     name: 'Prashanth',
+//     username: 'prash',
+//     email: 'ip11@prash.net'
+//   },
+//   {
+//     id: 4,
+//     name: 'Patricia Lebsack',
+//     username: 'Karianne',
+//     email: 'Julianne.OConner@kory.org'
+//   },
+//   {
+//     id: 5,
+//     name: 'Chelsey Dietrich',
+//     username: 'Kamren',
+//     email: 'Lucio_Hettinger@annie.ca'
+//   },
+//   {
+//     id: 6,
+//     name: 'Mrs. Dennis Schulist',
+//     username: 'Leopoldo_Corkery',
+//     email: 'Karley_Dach@jasper.info'
+//   },
+//   {
+//     id: 7,
+//     name: 'Kurtis Weissnat',
+//     username: 'Elwyn.Skiles',
+//     email: 'Telly.Hoeger@billy.biz'
+//   },
+//   {
+//     id: 8,
+//     name: 'Nicholas Runolfsdottir V',
+//     username: 'Maxime_Nienow',
+//     email: 'Sherwood@rosamond.me'
+//   },
+//   {
+//     id: 9,
+//     name: 'Glenna Reichert',
+//     username: 'Delphine',
+//     email: 'Chaim_McDermott@dana.io'
+//   },
+//   {
+//     id: 10,
+//     name: 'Clementina DuBuque',
+//     username: 'Moriah.Stanton',
+//     email: 'Rey.Padberg@karina.biz'
+//   }
+// ];
